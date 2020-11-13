7879cef2ca4968f7213207b827a5cb81127a312a
Fuxing Loh <git@fuxing.dev>
2020-11-13 19:23:31 +0800

    Fixed prettier lint errors Added pre-commit hooks

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/vue-horizontal-list/'
-    : '/',
-  outputDir: 'gh-pages'
-}
+  publicPath:
+    process.env.NODE_ENV === "production" ? "/vue-horizontal-list/" : "/",
+  outputDir: "gh-pages",
+};
